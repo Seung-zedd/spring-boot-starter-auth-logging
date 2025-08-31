7326afce1aee3a9022f6500b0bc4b4a6350cd77b
seung-je <csz0209@gmail.com>
2025-09-01 01:06:30 +0900

    Initial commit

---
+++ src/main/java/com/example/springboot_starter_auth/SpringbootStarterAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_starter_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStarterAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStarterAuthApplication.class, args);
+	}
+
+}
